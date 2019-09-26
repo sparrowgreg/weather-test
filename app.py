6e0cc80d5e4ae18bf86b39f9919e8795fa4b2ac4
sparrowgreg <55734993+sparrowgreg@users.noreply.github.com>
2019-09-26 12:15:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def hello():
 
 
 if __name__ == "__main__":
-        port = int(os.environ.get("PORT", 5000))
-        app.run(host='0.0.0.0', port=port)
-        
+        #port = int(os.environ.get("PORT", 5000))
+        #app.run(host='0.0.0.0', port=port)
+        app = Flask(__name__)
+        app.run(environ.get('PORT'))
         
