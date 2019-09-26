f5edbf1e6f589974b62f08a8724db49cff6c4361
sparrowgreg <55734993+sparrowgreg@users.noreply.github.com>
2019-09-26 12:12:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
+import os
+from pprint import pprint
+import requests
+from flask import request #Import flask requests
+from flask import render_template #Import flask render templates
+import json, urllib #import api modules
+import time #Imporing time in darlek voice
+import urllib.request
+
 from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return "Hello World!"
+    url = "http://api.openweathermap.org/data/2.5/weather?q=Athens,gr&APPID=e21f33558fd40ca1efd2be1eb27de5d4"
+    response = urllib.request.urlopen(url) #Download Json
+    data = json.loads(response.read()) #Parse json
+    return("Current Weather in " +data['name']+" "+"status: "+ data['weather'][0]['description'] +", "+ " Temperature: " + str(data['main']['temp'])) #Debug infomation
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+        port = int(os.environ.get("PORT", 5000))
+        app.run(host='0.0.0.0', port=port)
+        
+        
